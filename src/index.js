5158771c64abe1fa99744db28f3a2e05a0cb3568
Lindsay Warr <iamalion@gmail.com>
2023-08-28 13:33:32 -0700

    DRY code
    Co-authored-by: Jase Grable <jase.grable@gmail.com>
    Co-authored-by: Mike Workman <workmanmcr@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-//  function CoinCounter(amount) {
-//   const coinValues = [
-//     {quarters: 25}, 
-//     {dimes: 10}, 
-//     {nickles:5}, 
-//     {pennies:1} 
-// ];
-  
-function calculateChange(amount, coins = [25, 10, 5, 1], index = 0) {
+
+  function calculateChange(amount, coins = [25, 10, 5, 1], index = 0) {
     if (amount === 0) {
         return [];
     }
 
     if (index >= coins.length) {
-        return null; // No combination of coins can make up the amount
+        return null;
     }
 
     const currentCoin = coins[index];
     const maxCount = Math.floor(amount / currentCoin);
 
-    for (let count = maxCount; count >= 0; count--) {
-        const remainingAmount = amount - count * currentCoin;
+    const validChange = findValidChange(amount, coins, index, maxCount);
+    return validChange || null;
+  }
+    function findValidChange(amount, coins, index, maxCount){
+      const currentCoin = coins[index];
+    
+    return [...Array(maxCount + 1)].map((_, count) => {
+      const remainingAmount = amount - count * currentCoin;
         const nextChange = calculateChange(remainingAmount, coins, index + 1);
-
-        if (nextChange !== null) {
-            return [count, ...nextChange];
-        }
-    }
-
-    return null; // No combination of this coin works, try the next one
-}
+        return nextChange !== null ? [count, ...nextChange] : null;
+    }).find(change => change !== null);
+  }
 
 function printChange(change) {
     const coins = ["quarters", "dimes", "nickels", "pennies"];
@@ -40,7 +34,7 @@ function printChange(change) {
 }
 
 const amount = parseFloat(prompt("Enter the dollar amount: "));
-const change = calculateChange(amount * 100); // Convert to cents
+const change = calculateChange(amount * 100);
 if (change === null) {
     console.log("Cannot make change with available coins.");
 } else {
